7f15a67e687de1bc505f0dcc42712df82f5e1341
222835 <al222835@alumnos.uacj.mx>
2026-07-27 23:06:54 -0600

    feat: new Usuarios table

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,3 +2,4 @@ from app.models.organizacion import Organizacion
 from app.models.programa import Programa
 from app.models.colonia import Colonia
 from app.models.eventos import Evento
+from app.models.usuario import Usuario
